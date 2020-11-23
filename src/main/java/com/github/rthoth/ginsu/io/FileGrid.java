ecda91f0a008a4e77736419a57b77595144239a0
rthoth! <ronaldo.asilva@gmail.com>
2020-11-23 09:13:58 -0300

    Savepoint.

--- src/main/java/com/github/rthoth/ginsu/io/FileGrid.java
+++ src/main/java/com/github/rthoth/ginsu/io/FileGrid.java
@@ -8,22 +8,34 @@ import org.locationtech.jts.io.InputStreamInStream;
 import org.locationtech.jts.io.OutputStreamOutStream;
 import org.locationtech.jts.io.WKBReader;
 import org.locationtech.jts.io.WKBWriter;
+import org.pcollections.HashTreePMap;
+import org.pcollections.PMap;
 
 import java.io.*;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class FileGrid<G extends Geometry> extends Grid<G> {
 
-    private final WKBWriter writer;
-    private final ReentrantLock lock = new ReentrantLock();
+    //    private final ReentrantLock lock = new ReentrantLock();
+    private final PMap<Key, ReentrantLock> locks;
     private final File directory;
-    private final WKBReader reader;
+    private final int outputDimension;
+    private final boolean includeSRID;
+    private final GeometryFactory factory;
 
     public FileGrid(int width, int height, File directory, GeometryFactory factory, int outputDimension, boolean includeSRID) {
         super(width, height);
         this.directory = directory;
-        writer = new WKBWriter(outputDimension, includeSRID);
-        reader = new WKBReader(factory);
+        this.outputDimension = outputDimension;
+        this.includeSRID = includeSRID;
+        this.factory = factory;
+        var locks = HashTreePMap.<Key, ReentrantLock>empty();
+        for (var x = 0; x < width; x++) {
+            for (var y = 0; y < height; y++) {
+                locks = locks.plus(new Key(x, y), new ReentrantLock());
+            }
+        }
+        this.locks = locks;
     }
 
     public FileGrid(int width, int height, File directory, GeometryFactory factory) {
@@ -33,16 +45,20 @@ public class FileGrid<G extends Geometry> extends Grid<G> {
     @SuppressWarnings("unchecked")
     @Override
     protected G _get(int x, int y) {
-        lock.lock();
+        acquire(x, y);
         try {
             try (var input = new FileInputStream(new File(directory, x + File.separator + y))) {
-                return (G) reader.read(new InputStreamInStream(input));
+                return (G) newReader().read(new InputStreamInStream(input));
             } catch (Exception e) {
                 throw new GinsuException.IllegalState(String.format("It's impossible to read (%d, %d) geometry!", x, y), e);
             }
         } finally {
-            lock.unlock();
+            release(x, y);
         }
+    }
+
+    private void acquire(int x, int y) {
+        locks.get(new Key(x, y)).lock();
     }
 
     @Override
@@ -70,28 +86,53 @@ public class FileGrid<G extends Geometry> extends Grid<G> {
         throw new GinsuException.Unsupported();
     }
 
-    public <T> void updateWith(Grid<T> source, Combiner<G, T> combiner) {
+    private WKBReader newReader() {
+        return new WKBReader(factory);
+    }
+
+    private WKBWriter newWriter() {
+        return new WKBWriter(outputDimension, includeSRID);
+    }
+
+    private void release(int x, int y) {
+        locks.get(new Key(x, y)).unlock();
+    }
+
+    public FileGrid<G> update(int x, int y, G geometry) {
+        if (x >= 0 && y >= 0 && x < width && y < height) {
+            write(geometry, x, y, true);
+            return this;
+        } else {
+            throw new GinsuException.IllegalArgument("");
+        }
+    }
+
+    public <T extends G> void updateWith(Grid<T> source) {
         if (source.getWidth() == width && source.getHeight() == height) {
-            lock.lock();
-            try {
-                for (var e : source.iterable()) {
-                    write(combiner.combine(e.x, e.y, _get(e.x, e.y), e.value), e.x, e.y, false);
-                }
-            } finally {
-                lock.unlock();
+            for (var e : source.iterable()) {
+                write(e.value, e.x, e.y, true);
             }
         } else {
             throw new GinsuException.IllegalArgument("Different grids size!");
         }
+    }
 
+    public <T> void updateWith(Grid<T> source, Combiner<G, T> combiner) {
+        if (source.getWidth() == width && source.getHeight() == height) {
+            for (var e : source.iterable()) {
+                write(combiner.combine(e.x, e.y, _get(e.x, e.y), e.value), e.x, e.y, false);
+            }
+        } else {
+            throw new GinsuException.IllegalArgument("Different grids size!");
+        }
     }
 
     private void write(Geometry geometry, int x, int y, boolean mkdir) {
-        lock.lock();
+        acquire(x, y);
         try {
             final var file = new File(directory, (x + File.separator + y));
             try (var output = new FileOutputStream(file)) {
-                writer.write(geometry, new OutputStreamOutStream(output));
+                newWriter().write(geometry, new OutputStreamOutStream(output));
                 output.flush();
             } catch (FileNotFoundException e) {
                 if (mkdir && file.getParentFile().mkdirs()) {
@@ -103,7 +144,7 @@ public class FileGrid<G extends Geometry> extends Grid<G> {
                 throw new GinsuException.IllegalState("Unexpected!", e);
             }
         } finally {
-            lock.unlock();
+            release(x, y);
         }
     }
 
@@ -115,5 +156,30 @@ public class FileGrid<G extends Geometry> extends Grid<G> {
     public interface Generator<G> {
 
         G generate(int x, int y);
+    }
+
+    private static class Key {
+        final int x;
+        final int y;
+
+        private Key(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof Key) {
+                var other = (Key) obj;
+                return x == other.x && y == other.y;
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        public int hashCode() {
+            return x * x + y * y;
+        }
     }
 }
